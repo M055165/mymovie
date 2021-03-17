eee97d5dc1c41997f60bfd4465227dc44b35ffa8
m055165 <michael50601@gmail.com>
2021-03-17 15:22:45 +0800

    0317 modify

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="app">
+        <navbar></navbar>
+        <login></login>
+        <myfooter></myfooter>
+    </div>
+</template>
+<style lang="scss" scoped>
+    * {
+    
+        color:white;
+    }
+</style>
+
+<script>
+import navbar from '../components/navbar'
+import myfooter from '../components/Footer'
+import login from '../components/login'
+export default {
+    components:{navbar,myfooter,login}
+}
+</script>
