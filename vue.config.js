c4e5cc80a855efb8d83f2087f6e516f335f1b68f
m055165 <michael50601@gmail.com>
2021-07-11 15:51:43 +0800

    deploy 0711

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,8 @@ module.exports = {
       ? '/mymovie/'
       : '/',
       devServer: {
+        open: true,
+        host: 'https://m055165.github.io/mymovie/',
         proxy: {
           '/api': {
             target: 'https://boiling-everglades-57930.herokuapp.com/', //对应自己的接口
