0dde124243a0c792f3232bd38bdd2a9c657b51ab
m055165 <michael50601@gmail.com>
2021-07-11 16:03:09 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -2,19 +2,7 @@ module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? '/mymovie/'
       : '/',
-      devServer: {
-        open: true,
-        host: 'https://m055165.github.io/mymovie333/',
-        proxy: {
-          '/api': {
-            target: 'https://boiling-everglades-57930.herokuapp.com/', //对应自己的接口
-            changeOrigin: true,
-            ws: true,
-            pathRewrite: {
-              '^/api': ''
-            }
-          }
-        }
-      }
+      devServer: { proxy: { '/api': { target: 'https://boiling-everglades-57930.herokuapp.com/', changeOrigin: true, ws: true, pathRewrite: { '^/api':  'https://m055165.github.io/mymovie/', } } }
+    }
   }
   
